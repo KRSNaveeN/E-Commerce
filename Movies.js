d1fe347be2923208a8a1f967134d4660af64e4dd
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-12-19 09:22:27 +0530

    Add files via upload

--- Movies.js
+++ Movies.js
@@ -1,25 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MoviesList from "./MoviesList";
-
+// let x = 0;
 const Movies = ()=>{
  const [movies, setMovies] = useState([]);
  const [loading, setLoading] = useState(false);
+ const [error, setError] = useState(null);
+const [interval, setinter] = useState();
+const[count,setCount] =useState(0);
+// let x;
+//  useEffect(()=>(let x=0),[])
 
-  async function MoviesHandler(){
-    setLoading(true);
-     let response = await fetch("https://swapi.dev/api/films/");
-     let data = await response.json();
-     setLoading(false);
-    //  console.log(data.results);
-     setMovies(data.results);
-    }
+function removeHandler(){
+    clearInterval(interval);
+    console.log("removed");
+    
+}
+useEffect(()=>{clearInterval(interval)}, [count]);
+
+function MoviesHandler(){
+ setinter(setInterval(async () => {
+    console.log("called every 5 sec");
+        setLoading(true);
+        try{
+        let response = await fetch("https://swapi.dev/api/film/");
+        if(!response.ok){
+            throw new Error("trying to reconnect");
+            
+        }
+        let data = await response.json();
+        setCount((pre)=>pre+1)
+        setMovies(data.results);
+        }
+        catch(error)
+        {
+            
+            setError(error.message)
+        }
+        setLoading(false);
+    }, 5000));
+};
+
+// if(count >1){
+//     console.log(interval);
+//    clearInterval(interval);
+// }
+
+
+
     return <>
     <section style={{textAlign:"center"}}>
         <button  onClick={MoviesHandler}>Fetch Movies</button>
     </section>
     <section>
-        {loading&& <h2>loading....</h2>}
-        <MoviesList movies={movies}/>
+        {loading && <h2>loading....</h2>}
+        {!loading && error && <h2>{error}</h2>}
+        { error && <button onClick={removeHandler}>Cancel</button>}
+       {movies.length >0 && <MoviesList movies={movies}/>} 
     </section>
     </>;
 };
