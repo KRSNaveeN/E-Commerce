66ad1bad730f41ba7f94429e6f139c9f78c5e125
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-12-19 15:21:53 +0530

    Add files via upload

--- Movies.js
+++ Movies.js
@@ -1,60 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useCallback } from "react";
 import MoviesList from "./MoviesList";
 // let x = 0;
 const Movies = ()=>{
  const [movies, setMovies] = useState([]);
  const [loading, setLoading] = useState(false);
  const [error, setError] = useState(null);
-const [interval, setinter] = useState();
-const[count,setCount] =useState(0);
-// let x;
-//  useEffect(()=>(let x=0),[])
 
-function removeHandler(){
-    clearInterval(interval);
-    console.log("removed");
-    
-}
-useEffect(()=>{clearInterval(interval)}, [count]);
-
-function MoviesHandler(){
- setinter(setInterval(async () => {
-    console.log("called every 5 sec");
-        setLoading(true);
-        try{
-        let response = await fetch("https://swapi.dev/api/film/");
+ let moviesHandler =  useCallback (async () => {
+      setLoading(true);
+      try{
+        let response = await fetch("https://swapi.dev/api/films/");
         if(!response.ok){
             throw new Error("trying to reconnect");
-            
         }
         let data = await response.json();
-        setCount((pre)=>pre+1)
-        setMovies(data.results);
+         setMovies(data.results);
         }
         catch(error)
         {
-            
             setError(error.message)
         }
-        setLoading(false);
-    }, 5000));
-};
+        setLoading(false)
+    },[movies]);
 
-// if(count >1){
-//     console.log(interval);
-//    clearInterval(interval);
-// }
 
+useEffect(()=>{
+   console.log("hell mno");
+   moviesHandler();
+},[])
 
 
     return <>
     <section style={{textAlign:"center"}}>
-        <button  onClick={MoviesHandler}>Fetch Movies</button>
+        hello
     </section>
     <section>
         {loading && <h2>loading....</h2>}
         {!loading && error && <h2>{error}</h2>}
-        { error && <button onClick={removeHandler}>Cancel</button>}
+        {/* { error && <button onClick={removeHandler}>Cancel</button>} */}
        {movies.length >0 && <MoviesList movies={movies}/>} 
     </section>
     </>;
@@ -62,10 +45,3 @@ function MoviesHandler(){
 
 export default Movies;
 
-//  let updatedmovies = data.map((item)=>{
-    //     return {
-    //         id : item.episode_id,
-    //         title : item.opening_crawl,
-    //         date : item.release_date
-    //     }
-    //  })
